f5f1832f6f922dbd40896888a31632e4e2d19aa7
Hervin Efrain Garcia Cardona <hervin_rambito@hotmail.com>
2020-02-23 21:13:18 +0000

    add new collection at basedata

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra');
 const path = require('path');
-const { Image } = require('../models/image');
+const { Image } = require('../models');
 const { randomNumber } = require('../helpers/lins');
 
 const ctrl = {};
@@ -10,49 +10,47 @@ ctrl.index = (req, res) => {
 
 };
 
-ctrl.create = async (req, res) => {
-    const saveImage = async () => {
-      const imgUrl = randomNumber();
-      const images = await Image.find({ filename: imgUrl });
-      if (images.length > 0) {
-        saveImage()
+ctrl.create =  (req, res, next) => {
+  const saveImage = async () => {
+    const imgUrl = randomNumber();
+    const images = await Image.find({ filename: imgUrl });
+    if (images.length > 0) {
+      saveImage();
+    } else {
+      const imageTempPath = req.file.path;
+      const ext = path.extname(req.file.originalname).toLowerCase();
+      const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
+
+      if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+        // you wil need the public/temp path or this will throw an error
+        await fs.rename(imageTempPath, targetPath);
+        const newImg = new Image({
+          title: req.body.title,
+          filename: imgUrl + ext,
+          description: req.body.description
+        });
+        const imageSaved = await newImg.save();
+        res.send('works');
       } else {
-        // Image Location
-        const imageTempPath = req.file.path;
-        const ext = path.extname(req.file.originalname).toLowerCase();
-        const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
-  
-        // Validate Extension
-        if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
-          // you wil need the public/temp path or this will throw an error
-          await fs.rename(imageTempPath, targetPath);
-          const newImg = new Image({
-            title: req.body.title,
-            filename: imgUrl + ext,
-            description: req.body.description
-          });
-          const imageSaved = await newImg.save();
-          res.send('works');
-        } else {
-          await fs.unlink(imageTempPath);
-          res.status(500).json({ error: 'Only Images are allowed' });
-        }
+        await fs.unlink(imageTempPath);
+        res.status(500).json({ error: 'Only Images are allowed' });
       }
-    };
-  
-    saveImage();
+    }
   };
 
-    ctrl.like = (req, res) => {
+  saveImage();
+};
 
-    };
+ctrl.like = (req, res) => {
 
-    ctrl.comment = (req, res) => {
+};
 
-    };
+ctrl.comment = (req, res) => {
 
-    ctrl.remove = (req, res) => {
+};
 
-    };
+ctrl.remove = (req, res) => {
 
-    module.exports = ctrl;
+};
+
+module.exports = ctrl;
--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const { database } = require('./keys');
-
-mongoose.connect(database.URI, {
+mongoose.set('useFindAndModify', false);
+mongoose.connect('mongodb://localhost/instagram', {
+    useCreateIndex: true,
     useNewUrlParser: true
 })
 
 .then(db => console.log('db is connected'))
-.catch(err => console.log(err));
+.catch(err => console.error(err));
